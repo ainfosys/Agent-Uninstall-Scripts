8f05b83a85c1847e0b619cc9dd6db27db26bf133
Ryan <61989372+Alliance-Ryan@users.noreply.github.com>
2026-06-05 14:48:40 -0400

    Refactor Zorus uninstallation script
    Simplified the uninstallation process by removing password handling.

---
+++ Zorus Uninstall.ps1
@@ -0,0 +1,20 @@
+if ([Enum]::GetNames([System.Net.SecurityProtocolType]) -contains 'Tls12')
+{
+	[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+}
+else
+{
+	[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+}
+
+$source = "http://static.zorustech.com.s3.amazonaws.com/downloads/ZorusAgentRemovalTool.exe";
+$destination = "$env:TEMP\ZorusAgentRemovalTool.exe";
+
+Write-Host "Downloading Zorus Agent Removal Tool..."
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile($source, $destination)
+Write-Host "Uninstalling Zorus Deployment Agent..."
+Start-Process -FilePath $destination -ArgumentList "-s" -Wait
+Write-Host "Removing temporary files..."
+Remove-Item -recurse $destination
+Write-Host "Removal complete."
